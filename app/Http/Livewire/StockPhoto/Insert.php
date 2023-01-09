4bef0a9a384801af77c1fbc30477cddfc70ffc63
farros shier <farros@tribunnews.com>
2023-01-09 09:56:25 +0700

    update store image thumb

--- app/Http/Livewire/StockPhoto/Insert.php
+++ app/Http/Livewire/StockPhoto/Insert.php
@@ -9,6 +9,7 @@ use App\Models\StockPhoto;
 use App\Models\Category;
 use App\Models\Subcategory;
 use Image;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Validation\Rule; 
 
 class Insert extends Component 
@@ -43,26 +44,36 @@ class Insert extends Component
 		
 		];
 
-		// $image = $request->file('image');
-		$image = $this->stock_photo;
-
-		// dd($image->getClientOriginalName());
-
-        // $input['imagename'] = time().'.'.$image->extension();
-        $input['imagename'] = strtolower(str_replace(" ", "-", $image->getClientOriginalName())).'.'.$image->extension();
+		$image = $this->stock_photo;    
+        $input['imagename'] = strtolower(str_replace(" ", "-", $this->foto_caption)).'.'.$image->extension();
 		
-		$destinationPathThumb = public_path('Stock_Photo\thumbnail');
+		// $destinationPathThumb = public_path('Stock_Photo\thumbnail');
+		// $destinationPathThumb = storage_path('Stock_Photo\thumbnail');
+
 		
-        $img = Image::make($image->path());
-        $img->resize(240, 240, function ($constraint) {
-            $constraint->aspectRatio();
-        })->save($destinationPathThumb.'\\'.$input['imagename']);
+        // $img = Image::make($image->path());
+        // $img->resize(240, 240, function ($constraint) {
+        //     $constraint->aspectRatio();
+        // })->save($destinationPathThumb.'\\'.$input['imagename']);
+
+		
+		// $img = Image::make($image->path())->resize(240, 240, function ($constraint) {
+        //     $constraint->aspectRatio();
+        // });
+		// $img->save('Stock_Photo/Thumbnail', $input['imagename'], 100);
+
+		$new_img = Image::make($image->getRealPath())->resize(240, 160)->resizeCanvas(240, 160);
+
+		// save file with medium quality
+		$new_img->save(storage_path('app\public\Stock_Photo\Thumbnail\\'.$input['imagename']), 80);
+
+		// Storage::putFileAs('public/Stock_Photo/Thumbnail' . $input['imagename'], (string)$image->encode('jpg', 95), $input['imagename']);
+		// Storage::disk('local')->putFileAs('public/Stock_Photo/Thumbnail', $img, $input['imagename']);
    
-        // $destinationPathFull = public_path('Stock_Photo\images');
-        // $image->move($destinationPathFull, $input['imagename']);
 
-		$this->stock_photo->storePubliclyAs('Stock_Photo\images',$input['imagename']);
-		// $this->stock_photo->store('toPath', ['disk' => 'public']);
+		// $this->stock_photo->storePubliclyAs('Stock_Photo\images',$input['imagename']);
+		// $this->stock_photo->store('Stock_Photo/Images', ['disk' => 'public']);
+		$this->stock_photo->storeAs('public/Stock_Photo/Images', $input['imagename']);
 
 
 		$data 				= new StockPhoto();
@@ -74,19 +85,6 @@ class Insert extends Component
         $data->status		= 1;
         $data->save();
 
-
-
-		
-		// if($this->validate_form_5){ 
-		// 	$counting =  get_setting('counting_no_anggota_new')+1;
-		// 	update_setting('counting_no_anggota_new',$counting);
-		// 	$this->emit('save_rekomendasi',['num'=>5,'id'=>$data->id,'no_anggota'=>$counting]);
-		// }
-		// $this->is_success =true;
-		
-		// $messageWa  = "Kepada Yth Ibu/Bpak ".$data->name.",\n\nTerima kasih telah mendaftar sebagai Anggota di Yayasan Kematian Santa Maria, \nNomor Anggota : *".$data->no_anggota_platinum."*\n. Silahkan login dengan menggunakan username :". $data->no_anggota_platinum ."\n dan password {$password} \n";
-        // $messageWa .= 'Masa Tunggu Klaim : '. date('d F Y',strtotime($data->masa_tenggang));
-        // sendNotifWa($messageWa, $this->phone_number);
         
 		session()->flash('message-success',__('Photo berhasil disimpan'));
 
